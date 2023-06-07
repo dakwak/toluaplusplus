358c97a7c1bc6e4c6021e983d80d081abc11b06e
root <dakwak@dakwak.com>
2023-06-07 12:35:08 +0000

    init commit with fixes

---
+++ debian/custom.py
@@ -0,0 +1,9 @@
+CCFLAGS = ['-I/usr/include/lua5.1', '-O2', '-ansi', '-Wall', '-g', '-D_REENTRANT', '-fPIC']
+LIBPATH = ['/usr/lib']
+LIBS = ['lua5.1']
+prefix = '/usr'
+build_dev = 1
+#shared = 1
+tolua_bin = 'tolua++5.1'
+tolua_lib = 'tolua++5.1'
+TOLUAPP = 'tolua++5.1'
---
+++ src/bin/toluabind.h
@@ -0,0 +1,8 @@
+/*
+** Lua binding: tolua
+** Generated automatically by tolua++-1.0.92 on Wed Jun  7 12:05:19 2023.
+*/
+
+/* Exported function */
+TOLUA_API int  tolua_tolua_open (lua_State* tolua_S);
+
